4a158a8d099f5cf7a1cde4b3e5629d70698a6080
david rolfs and nick wise <nick@>
2017-05-01 10:27:27 -0700

    add second array that takes items from the first array and puts them in another

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
   $("form#faveThings").submit(function(event){
     event.preventDefault();
-    // var inputList = $("#faveList").val();
-    // var array = [];
 
     var array = $("#faveList").val().split(" ");
-
-    // var split = inputList.split(" ");
-    // var reverse = split.reverse();
-    //  array.push(inputList);
+    //reverse array
+    //array.reverse()
     console.log(array);
-    $("#show").text(array.reverse());
+    var one = array[1];
+    var two = array[2];
+    var three = array [3];
+    var array2 = []
+    array2.push(one, two, three)
+    console.log(array2)
+    $("ul").text(one);
+    $("ul").text(two);
 
 
   });
