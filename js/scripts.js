eb41e16361cc64ff998393d2acd0ad1ba5b3be58
david rolfs and nick wise <david@>
2017-05-01 09:59:58 -0700

    change original array so it becomes an array with splipt insted of pushing it into another array

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
   $("form#faveThings").submit(function(event){
     event.preventDefault();
-    var inputList = $("#faveList").val();
-    var array = [];
+    // var inputList = $("#faveList").val();
+    // var array = [];
 
-    var split = inputList.split(" ");
-    var reverse = split.reverse();
-    array.push(reverse);
+    var array = $("#faveList").val().split(" ");
+
+    // var split = inputList.split(" ");
+    // var reverse = split.reverse();
+    //  array.push(inputList);
     console.log(array);
-    $("#show").text(array);
+    $("#show").text(array.reverse());
 
 
   });
